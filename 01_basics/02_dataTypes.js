71330ca3bd667d228e8b6e409cb0cafe86765735
qureshihamza0001 <qureshihamza0001@gmail.com>
2026-07-31 11:02:50 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+// "use strict", treat all js code as newer version
+
+// alert( 3 + 3)we are using nodejs not browser
+
+console.log(3
+    +
+     3)  // code readiblity should be high
+
+     console.log("Hamza")
+
+
+     let name = "hamza" 
+     let age = 18
+     let isLoggedIn = false
+     let state;
+
+    //  number => 2 to the power 53
+    // bigint
+    // string => ""
+    // boolean => true/false 
+    // null=> standalone value 
+    // undefined=>
+
+        // object
+
+        console.log(typeof undefined);  //output will be undefined only
+        
+console.log(typeof null); //output will be object
+
+    
