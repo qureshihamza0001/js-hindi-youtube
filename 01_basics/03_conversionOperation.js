46e5dabb4a053f72782765efb2f21bea3eb06069
qureshihamza0001 <qureshihamza0001@gmail.com>
2026-07-31 11:54:11 +0000

    conversion of operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+let score = "hamza"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => is converted into 33
+//"33abc" => not a number NaN
+// true=> 1 ; false=> 0
+
+let isLoggedIn = "hamza"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+//  "hamza"=> true
+
+let somenumber  = 33
+let stringNumber = String(somenumber) 
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************* Operations *************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue); 
+
+let str1 = "hello"
+let str2 =  " hamza"
+let str3 = str1 + str2
+console.log(str3);
+
+
+
+
+
