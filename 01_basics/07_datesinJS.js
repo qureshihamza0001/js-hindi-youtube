828d3966554e5253d137c4430243c455577d24a3
qureshihamza0001 <qureshihamza0001@gmail.com>
2026-08-05 15:44:23 +0000

    dates

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,16 @@
+// Dates
+
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString);
+// console.log(myDate.toDateString);
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23 )
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3 )
+// let myCreatedDate = new Date("2023-01-14" )
+let myCreatedDate = new Date("01-14-2023" )
+console.log(myCreatedDate.toLocaleString());
+
+
+
