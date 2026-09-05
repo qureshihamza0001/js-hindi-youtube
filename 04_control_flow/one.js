a9182584e46704801e3cd183db931e8e0ebfcf4b
qureshihamza0001 <qureshihamza0001@gmail.com>
2026-09-05 07:12:03 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,13 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 41){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+
+// < , > , <= , >= , == , !=, === , !==
