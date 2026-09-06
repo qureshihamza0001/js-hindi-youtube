70848196501ca7a4f5789ee24fbcc7b51ce683ad
qureshihamza0001 <qureshihamza0001@gmail.com>
2026-09-06 08:53:25 +0000

    control flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,6 +1,6 @@
 // if
-const isUserloggedIn = true
-const temperature = 41
+// const isUserloggedIn = true
+// const temperature = 41
 
 // if ( temperature === 41){
 //     console.log("less than 50");
@@ -11,3 +11,45 @@ const temperature = 41
 // console.log("Execute");
 
 // < , > , <= , >= , == , !=, === , !==
+
+// const score = 200
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`user power: ${power}`);
+    
+// }
+//     console.log(`user power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");.....we should avoid writing these type of code
+
+ 
+    // if (balance < 500) {
+    //     console.log("less than 500");
+        
+    // }else if (balance < 750) {
+    //     console.log("less than 750");
+        
+    // }else if (balance < 900) {
+    //     console.log("less than 900");
+        
+    // }else {
+    //     console.log("less than 1200");
+
+    // }
+
+    // const userLoggedIn = true
+    // const debitcard = true
+    // const loggedInFromGoogle = false
+    // const loggedInFromEmail = true
+
+    // if (userLoggedIn && debitcard && 2==3) {
+    //     console.log("Allow to buy courses");
+        
+    // }
+    // if (loggedInFromGoogle || loggedInFromEmail) {
+    //     console.log("User Logged In");
+        
+    // }
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,35 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }....basic synatx of switch
+
+const month = "March"
+
+switch (month) {
+    case "Jan":
+        console.log("January");
+        
+        break;
+    case "Feb":
+        console.log("February");
+        
+        break;
+    case "March":
+        console.log("March");
+        
+        break;
+    case "April":
+        console.log("April");
+        
+        break;
+    
+
+    default:
+        console.log("defaul case match");
+        
+        break;
+}
